5f23344e4de9047ad836c691076424b81d02595c
Tyler Kranig <tylerkranig@gmail.com>
2020-01-22 14:55:45 -0600

    added initial runs for testing

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # toxicity_report
 A tool to calculate the most toxic players in Chill Dota League
+
+This uses a tensor flow library https://github.com/tensorflow/tfjs-models/tree/master/toxicity
+It also runs a tool that allows python to execute javascript
--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 import requests
+import sys
 import json
 from Naked.toolshed.shell import execute_js, muterun_js
 
@@ -6,3 +7,5 @@ from Naked.toolshed.shell import execute_js, muterun_js
 session = requests.session()
 
 print(json.loads(session.get("https://api.opendota.com/api/matches/5202117012").text)["chat"])
+response = execute_js('flow.js')
+print(response)
